a0990b253600ed328d8b0e421e2c1445afe8adfa
Muhammed Ashique <89317428+Bendalledj@users.noreply.github.com>
2022-10-24 01:48:31 +0530

    Add files via upload

---
+++ Diwali.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#Install xcowsay & mplayer utility
+
+sudo apt-get install xcowsay
+sudo apt-get install mplayer
+
+figlet "Diwali"
+echo "created by @neha   ver:-1.0         23/10/2022"
+#LinkedIn: https://www.linkedin.com/in/neha-gautam-20235320a/
+# playing diwali music
+
+mplayer diwali.mp3
+
+# modified command of xcowsay
+
+xcowsay --image=gb3.jpg "Happy Diwali to You. May this Diwali enlighten your success path with colourfull lights. Wish you a loving Diwali to you and your family members and loving ones."
+
